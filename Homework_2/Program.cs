ec6d4412c469b15cc6acd010e3f1d7fd150368e8
Victoria Artyushkina <victoriaartyushkina@gmail.com>
2022-10-06 17:32:24 +0200

    Amend the solution to the 3rd task

--- Homework_2/Program.cs
+++ Homework_2/Program.cs
@@ -1,19 +1,23 @@
 ﻿/* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
 и проверяет, является ли этот день выходным. */
 
-int rand;
+int N;
 
-rand = new Random().Next(1, 8);
+Console.WriteLine("Введите число: (1 - понедельник, 2 - вторник, ..., 7 - воскресенье)");
+int.TryParse(Console.ReadLine()!, out N);
 
-Console.WriteLine(rand);
 
-if (rand == 6 || rand == 7)
+if (N == 6 || N == 7)
 {
     Console.WriteLine("да");
 }
-else
+else if (N >= 1 && N < 6) 
 {
     Console.WriteLine("нет");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод");
 }
 
 
